8681dd2f117f677a323990fb0fe8a9d2b9a6c5d4
Manuel <manuel.panizzo@univtec.com>
2020-08-20 11:09:21 -0300

    added email forgot password

---
+++ application/views/templates/email_forgot_password_spanish.php
@@ -0,0 +1,22 @@
+
+<div style="font-family: calibri,sans-serif;">
+    <h4>Querido/a <?php echo $name.","; ?></h4>
+
+    <p>
+        Tu contraseña ha sido actualizada. La nueva contraseña es
+    </p>
+    <p style="font-size:18px;">
+        <?php echo $password; ?>
+
+    </p>
+
+    <br>
+    <p>
+        Gracias,<br>
+        The 1Spot Media Team<br>
+        <br>
+
+        <span style="font-size: 12px">Por favor no responda a este mensaje. Se envió desde una dirección de correo electrónico automática.</span><br>
+
+    </p>
+</div>
